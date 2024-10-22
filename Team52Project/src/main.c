bd2b594b58534713e869714237cb744bcf653794
Demir Kaya <demirk1776@gmail.com>
2024-10-22 14:46:48 -0400

    serial out working

--- Team52Project/src/main.c
+++ Team52Project/src/main.c
@@ -5,6 +5,21 @@
 extern void autotest();
 extern void internal_clock();
 
+void setup_serial(void)
+{
+    RCC->AHBENR |= 0x00180000;
+    GPIOC->MODER  |= 0x02000000;
+    GPIOC->AFR[1] |= 0x00020000;
+    GPIOD->MODER  |= 0x00000020;
+    GPIOD->AFR[0] |= 0x00000200;
+    RCC->APB1ENR |= 0x00100000;
+    USART5->CR1 &= ~0x00000001;
+    USART5->CR1 |= 0x00008000;
+    USART5->BRR = 0x340;
+    USART5->CR1 |= 0x0000000c;
+    USART5->CR1 |= 0x00000001;
+}
+
 void setup_tim3(void) {
     RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
     RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
@@ -49,10 +64,17 @@ void setup_tim3(void) {
 
 
 int main(void) {
+    setup_serial();
     internal_clock();
+    
     autotest();
-    sprintf(stdout, "\nstart\n");
+    printf("\nstart\n");
     setup_tim3();
-    sprintf(stdout, "\nsuccess\n");
+    printf("\nsuccess\n");
+
+    while(1) {
+        if ((USART5->ISR & USART_ISR_RXNE))  // if receive buffer has some data in it
+            USART5->TDR = USART5->RDR;       // copy that data into transmit buffer.
+    }
 }
 
