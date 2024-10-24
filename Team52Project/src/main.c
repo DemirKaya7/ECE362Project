0c9525d669e68f0c5368702622734a790cd2f90c
ebartcza <ebartcza@purdue.edu>
2024-10-24 18:30:49 -0400

    testing pwm

--- Team52Project/src/main.c
+++ Team52Project/src/main.c
@@ -25,14 +25,8 @@ void setup_tim3(void) {
     RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
 
     // enable pb[0, 1, 4, 5] in AF (for tim3 channels 1-4) 
-    GPIOB->MODER &= ~(GPIO_MODER_MODER0_0);
-    GPIOB->MODER |= GPIO_MODER_MODER0_1;
-    GPIOB->MODER &= ~(GPIO_MODER_MODER1_0);
-    GPIOB->MODER |= GPIO_MODER_MODER1_1;
-    GPIOB->MODER &= ~(GPIO_MODER_MODER4_0);
-    GPIOB->MODER |= GPIO_MODER_MODER5_1;
-    GPIOB->MODER &= ~(GPIO_MODER_MODER5_0);
-    GPIOB->MODER |= GPIO_MODER_MODER5_1;
+    GPIOB->MODER &= ~(GPIO_MODER_MODER0_0 | GPIO_MODER_MODER1 | GPIO_MODER_MODER4 | GPIO_MODER_MODER5);
+    GPIOB->MODER |= (GPIO_MODER_MODER0_1 | GPIO_MODER_MODER1_1 | GPIO_MODER_MODER4_1 | GPIO_MODER_MODER5_1);
 
     // specify AF1
     GPIOB->AFR[0] |= (1 << GPIO_AFRL_AFRL0_Pos);
@@ -41,8 +35,8 @@ void setup_tim3(void) {
     GPIOB->AFR[0] |= (1 << GPIO_AFRL_AFRL5_Pos);
 
     // set for frequency of 48000 KHz
-    TIM3->PSC = 48 - 1;
-    TIM3->ARR = 10000 - 1;
+    TIM3->PSC = 480 - 1;
+    TIM3->ARR = 1000 - 1;
 
     // enable PWM mode 1 and enable timer
     TIM3->CCMR1 |= (TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2);
@@ -54,11 +48,23 @@ void setup_tim3(void) {
     TIM3->CCER |= TIM_CCER_CC3E;
     TIM3->CCER |= TIM_CCER_CC4E;
     TIM3->CR1 |= TIM_CR1_CEN;
+
+
     // set CCR
-    TIM3->CCR1 = 5000;
-    TIM3->CCR2 = 2500;
-    TIM3->CCR3 = 1250;
-    TIM3->CCR4 = 625;
+    
+    TIM3->CCR1 = 250;
+    TIM3->CCR2 = 500;
+    TIM3->CCR3 = 750;
+    TIM3->CCR4 = 1000;
+    
+
+    /*
+    TIM3->CCR1 = 625;
+    TIM3->CCR2 = 1250;
+    TIM3->CCR3 = 2500;
+    TIM3->CCR4 = 5000;
+    */
+    
 }
 
 
