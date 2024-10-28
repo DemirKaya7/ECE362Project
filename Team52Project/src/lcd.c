a101f27e0b8c22023d9776ce986646c4b62f15ff
Akash Krishna Kumar <kumar556@ecelnx62.ecn.purdue.edu>
2024-10-28 16:07:17 -0400

    started code for lcd screen

---
+++ Team52Project/src/lcd.c
@@ -0,0 +1,80 @@
+#include "lcd.h"
+
+void init_spi() {
+    RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
+    RCC->AHBENR  |= RCC_AHBENR_GPIOAEN; 
+
+    GPIOA->MODER |= (GPIO_MODER_MODER15_1 | GPIO_MODER_MODER5_1 | GPIO_MODER_MODER7_1);
+    
+    GPIOA->AFR[0] &= 0x0f0fffff;
+    GPIOA->AFR[1] &= 0x0fffffff;
+
+    SPI1->CR1 &= ~SPI_CR1_SPE;
+    SPI1->CR1 |= SPI_CR1_BR;
+    
+    SPI1->CR2 |= SPI_CR2_DS_3 | SPI_CR2_DS_0;
+    SPI1->CR2 &= ~(SPI_CR2_DS_2 | SPI_CR2_DS_1);
+
+    SPI1->CR2 |= SPI_CR2_SSOE | SPI_CR2_NSSP;
+    SPI1->CR1 |= SPI_CR1_MSTR;
+    SPI1->CR2 |= SPI_CR2_TXDMAEN;
+
+    SPI1->CR1 |= SPI_CR1_SPE;
+}
+
+void send_command(uint8_t command) {
+    GPIOA->BSRR = (1 << 6) << 16; // DC low for command
+    GPIOA->BSRR = (1 << 4) << 16; // CS low
+    while (!(SPI1->SR & SPI_SR_TXE)); // Wait until TX buffer is empty
+    SPI1->DR = command;
+    while (!(SPI1->SR & SPI_SR_TXE)); // Wait until transmission is complete
+    GPIOA->BSRR = (1 << 4);           // CS high
+}
+
+void send_data(uint8_t data) {
+    GPIOA->BSRR = (1 << 6);       // DC high for data
+    GPIOA->BSRR = (1 << 4) << 16; // CS low
+    while (!(SPI1->SR & SPI_SR_TXE)); // Wait until TX buffer is empty
+    SPI1->DR = data;
+    while (!(SPI1->SR & SPI_SR_TXE)); // Wait until transmission is complete
+    GPIOA->BSRR = (1 << 4);           // CS high
+}
+
+void lcd_reset() {
+    GPIOA->BSRR = (1 << 3) << 16; // Set PA3 (RST) low
+    for (volatile int i = 0; i < 100000; i++); // Short delay
+    GPIOA->BSRR = (1 << 3);       // Set PA3 (RST) high
+}
+
+void init_lcd() {
+    lcd_reset();           // Reset the LCD
+    send_command(0x28);    // Display OFF
+    send_command(0x11);    // Exit Sleep mode
+    for (volatile int i = 0; i < 1000000; i++); // Short delay
+
+    // Initialization sequence specific to your LCD
+    send_command(0xCF);
+    send_data(0x00);
+    send_data(0xC1);
+    send_data(0x30);
+
+    // Additional commands for your LCD...
+    send_command(0x29); // Display ON
+}
+
+void draw_pixel(uint16_t x, uint16_t y, uint16_t color) {
+    // Set column address
+    send_command(0x2A);
+    send_data(x >> 8);
+    send_data(x & 0xFF);
+
+    // Set row address
+    send_command(0x2B);
+    send_data(y >> 8);
+    send_data(y & 0xFF);
+
+    // Write color data
+    send_command(0x2C);
+    send_data(color >> 8);
+    send_data(color & 0xFF);
+}
--- Team52Project/src/main.c
+++ Team52Project/src/main.c
@@ -4,6 +4,7 @@
 #include "stepper.h"
 #include "serial.h"
 #include "irsensor.h"
+#include "lcd.h"
 
 extern void autotest();
 extern void internal_clock();
@@ -11,17 +12,29 @@ extern void internal_clock();
 int main(void) {
     setup_serial();
     internal_clock();
-    IrSensor_Init();
+    // IrSensor_Init();
 
     autotest();
-    printf("setting up tim3\n");
-    setup_tim3();
-    printf("done setting up tim3\n");
+    // printf("setting up tim3\n");
+    // setup_tim3();
+    // printf("done setting up tim3\n");
 
-    while(1) {
-        printf("Turning CW\n");
-        turn_CW();
-        printf("Turning CCW\n");
-        turn_CCW();
+    // while(1) {
+    //     printf("Turning CW\n");
+    //     turn_CW();
+    //     printf("Turning CCW\n");
+    //     turn_CCW();
+    // }
+
+    init_spi();
+    lcd_reset();
+    init_lcd();
+    for (int i = 0; i < 240; i++) { 
+        for (int j = 0; j < 240; j++){
+            if (i == 0 && j == 0) {
+                printf("drawing\n");
+            }
+            draw_pixel(i,j,0x0f00);
+        }
     }
 }
