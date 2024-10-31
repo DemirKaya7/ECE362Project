1160f58369da5615ac3fa4317b6f94ad24043823
Demir Kaya <demirk1776@gmail.com>
2024-10-31 17:55:37 -0400

    Works perfect, cleaned up excess code and comments

--- Team52Project/src/main.c
+++ Team52Project/src/main.c
@@ -3,16 +3,13 @@
 #include <stdio.h>
 #include "stepper.h"
 #include "serial.h"
-// #include "irsensor.h"
+#include "irsensor.h"
 #include "lcd.h"
 
 extern void autotest();
 extern void internal_clock();
 
-int main(void) {
-    setup_serial();
-    internal_clock();
-
+void init_all(void) {
     RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
     RCC->AHBENR  |= RCC_AHBENR_GPIOBEN; 
 
@@ -27,7 +24,6 @@ int main(void) {
     GPIOB->AFR[0] &= ~(1 << GPIO_AFRL_AFRL5_Pos);
 
 
-    //init spi1 slow
     SPI1->CR1 &= ~SPI_CR1_SPE;
     SPI1->CR1 &= ~(SPI_CR1_BR);
     SPI1->CR2 |= SPI_CR2_DS_2 | SPI_CR2_DS_1 | SPI_CR2_DS_0;
@@ -38,20 +34,13 @@ int main(void) {
     SPI1->CR2 |= SPI_CR2_TXDMAEN;
 
     SPI1->CR1 |= SPI_CR1_SPE;
-    
-    // IrSensor_Init();
 
-    // autotest();
-    // printf("setting up tim3\n");
-    // setup_tim3();
-    // printf("done setting up tim3\n");
+}
 
-    // while(1) {
-    //     printf("Turning CW\n");
-    //     turn_CW();
-    //     printf("Turning CCW\n");
-    //     turn_CCW();
-    // }
+
+int main(void) {
+    setup_serial();
+    internal_clock();
 
     LCD_Setup();
     LCD_Clear(0x0);
@@ -60,31 +49,4 @@ int main(void) {
 
     printf("drawn\n");
 
-
-
-    // lcd_reset();
-    // init_lcd();
-    // draw_pixel(120, 160, 0x0);
-    // draw_pixel(120, 161, 0x0);
-    // draw_pixel(120, 162, 0x0);
-    // draw_pixel(120, 163, 0x0);
-    // draw_pixel(121, 160, 0x0);
-    // draw_pixel(121, 161, 0x0);
-    // draw_pixel(121, 162, 0x0);
-    // draw_pixel(121, 163, 0x0);
-    // draw_pixel(122, 160, 0x0);
-    // draw_pixel(122, 161, 0x0);
-    // draw_pixel(122, 162, 0x0);
-    // draw_pixel(122, 163, 0x0);
-    // printf("pixel drawn\n"); // pixel should be shown, but nothing is changing on the screen, this line is being printed
-    // spi1_setup_dma();
-    // spi1_enable_dma();
-    // for (int i = 0; i < 240; i++) { 
-    //     for (int j = 0; j < 240; j++){
-    //         if (i == 0 && j == 0) {
-    //             printf("drawing\n");
-    //         }
-    //         draw_pixel(i,j,0x0000);
-    //     }
-    // }
 }
